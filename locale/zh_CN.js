b3241e4065d498decd97e10a89c0f143b2ba1a3c
xiangruihua <13986295311@139.com>
2025-04-26 13:23:10 +0800

    add assets

---
+++ locale/zh_CN.js
@@ -0,0 +1,107 @@
+export default {
+  badge: "徽章",
+  open: "打开",
+  close: "关闭",
+  dismiss: "关闭",
+  confirmEdit: {
+      ok: "确定",
+      cancel: "取消"
+  },
+  dataIterator: {
+      noResultsText: "没有匹配的记录",
+      loadingText: "加载中..."
+  },
+  dataTable: {
+      itemsPerPageText: "每页条数:",
+      ariaLabel: {
+          sortDescending: "倒序",
+          sortAscending: "正序",
+          sortNone: "未排序",
+          activateNone: "激活以取消排序",
+          activateDescending: "激活以倒序",
+          activateAscending: "激活以正序"
+      },
+      sortBy: "排序"
+  },
+  dataFooter: {
+      itemsPerPageText: "每页条数:",
+      itemsPerPageAll: "全部",
+      nextPage: "下一页",
+      prevPage: "上一页",
+      firstPage: "第一页",
+      lastPage: "最后一页",
+      pageText: "{0}-{1} 共 {2}"
+  },
+  dateRangeInput: {
+      divider: "至"
+  },
+  datePicker: {
+      itemsSelected: "{0} 条选中",
+      range: {
+          title: "选择日期",
+          header: "输入日期"
+      },
+      title: "选择日期",
+      header: "输入日期",
+      input: {
+          placeholder: "输入日期"
+      }
+  },
+  noDataText: "没有数据",
+  carousel: {
+      prev: "上一视图",
+      next: "下一视图",
+      ariaLabel: {
+          delimiter: "轮播图 {0} 共 {1}"
+      }
+  },
+  calendar: {
+      moreEvents: "{0} 个更多",
+      today: "今天"
+  },
+  input: {
+      clear: "清除 {0}",
+      prependAction: "{0} 前置操作",
+      appendAction: "{0} 后置操作",
+      otp: "请输入OTP字符 {0}"
+  },
+  fileInput: {
+      counter: "{0} 个文件",
+      counterSize: "{0} 个文件 ({1} 总计)"
+  },
+  fileUpload: {
+      title: "拖拽文件到这里",
+      divider: "或",
+      browse: "浏览",
+  },
+  timePicker: {
+      am: "上午",
+      pm: "下午",
+      title: "选择时间",
+  },
+  pagination: {
+      ariaLabel: {
+          root: "分页导航",
+          next: "下一页",
+          previous: "上一页",
+          page: "跳到{0}页",
+          currentPage: "第{0}页, 当前页",
+          first: "第一页",
+          last: "最后一页",
+      }
+  },
+  stepper: {
+      next: "下一步",
+      prev: "上一步",
+  },
+  rating: {
+      ariaLabel: {
+          item: "评分 {0} 共 {1}"
+      }
+  },
+  loading: "加载中...",
+  infiniteScroll: {
+      loadMore: "加载更多",
+      empty: "无更多"
+  }
+}
